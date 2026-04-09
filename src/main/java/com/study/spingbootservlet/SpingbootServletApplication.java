3d3e08145ded94aca29a34a7b874f5f793955756
leeyonghee030 <leeyonghee030@gmail.com>
2026-04-09 20:43:27 +0900

    first commit

---
+++ src/main/java/com/study/spingbootservlet/SpingbootServletApplication.java
@@ -0,0 +1,13 @@
+package com.study.spingbootservlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingbootServletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/spingbootservlet/SpingbootServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.spingbootservlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootServletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
